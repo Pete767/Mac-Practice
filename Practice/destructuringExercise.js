f94dc4b475b79d97f50a29e6270e04fb482c2c8e
Pete Gonzales <petegonzales@Petes-Air.lan>
2023-01-19 22:45:25 -0600

    destructuring practice

---
+++ Practice/destructuringExercise.js
@@ -0,0 +1,38 @@
+8
+1846
+
+{yearNeptuneDiscovered: 1846, yearMarsDiscovered: 1659}
+
+your name is alejandro and you like purple
+
+your name is mellissa and you like green
+
+your name name is undefined and you like green
+
+maya 
+marisa 
+chi 
+
+raindrops on roses
+whiskeers on  kitens
+bright copper kettles, warm woolen mittens, brown paper pakages tied up with strings 
+
+10,30,20
+
+const obj = {
+    numbers: {
+      a: 1,
+      b: 2
+    }
+  };
+  
+  const {a,b} = obj.numbers
+
+
+  [arr[0], arr[1]] = [arr[1], arr[0]]
+
+
+  
+  raceResults(['Tom', 'Margaret', 'Allison', 'David', 'Pierre'])
+  
+  const raceResults = ([first, second, third, ...rest]) => ({first, second, third, rest})
